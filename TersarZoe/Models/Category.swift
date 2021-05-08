b8acf42ebccd5ba10ea1b0c6edfa33db9b47a5bc
Manju <manjunath.ramesh@philips.com>
2021-05-08 13:06:57 +0530

    Added new models

---
+++ TersarZoe/Models/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  TersarZoe
+//
+//  Created by Manjunath Ramesh on 08/05/21.
+//
+
+import Foundation
---
+++ TersarZoe/Models/Photo.swift
@@ -0,0 +1,8 @@
+//
+//  Photo.swift
+//  TersarZoe
+//
+//  Created by Manjunath Ramesh on 08/05/21.
+//
+
+import Foundation
