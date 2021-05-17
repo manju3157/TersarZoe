a36468c24a33117c4ac18e3e9b4fe0773b0657fe
Manju <manjunath.ramesh@philips.com>
2021-05-17 19:12:58 +0530

    Added About Us

--- TersarZoe/Settings/AboutUsViewController.swift
+++ TersarZoe/Settings/AboutUsViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class AboutUsViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var imgView: CircularImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        textView.setContentOffset(.zero, animated: false)
     }
-    */
-
+    @IBAction func cancelAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
